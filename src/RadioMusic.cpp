f54ab7273d447846879575cdc91ac98a6a766071
Christoph Scholtes <cschol2112@gmail.com>
2020-11-29 08:58:08 -0700

    Remove commented out code.

--- src/RadioMusic.cpp
+++ src/RadioMusic.cpp
@@ -345,11 +345,8 @@ struct RadioMusic : Module {
 	{
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 
-		//addParam(createParam<Davies1900hBlackKnob>(Vec(12, 49), module, RadioMusic::CHANNEL_PARAM, 0.0f, 1.0f, 0.0f));
 		configParam(CHANNEL_PARAM, 0.0f, 1.0f, 0.0f, "Channel");
-		//addParam(createParam<Davies1900hBlackKnob>(Vec(12, 131), module, RadioMusic::START_PARAM, 0.0f, 1.0f, 0.0f));
 		configParam(START_PARAM, 0.0f, 1.0f, 0.0f, "Start");
-		//addParam(createParam<PB61303>(Vec(25, 202), module, RadioMusic::RESET_PARAM, 0, 1, 0));
 		configParam(RESET_PARAM, 0.0f, 1.0f, 0.0f, "Reset");
 
 		currentPlayer = &audioPlayer1;
